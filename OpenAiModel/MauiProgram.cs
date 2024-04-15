248b430112bb423a6b8626565684fb262b5b14b7
GuardianEatos97 <5100158@mypillar7uwc.ac.za>
2024-04-15 15:45:42 +0200

    Program updated

--- OpenAiModel/MauiProgram.cs
+++ OpenAiModel/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using System.Runtime;
 
 namespace OpenAiModel
 {
@@ -14,12 +15,44 @@ namespace OpenAiModel
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
+            builder.RegisterServices()
+                .RegisterViewModels()
+                .RegisterViews();
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
+        }
+
+        public static MauiAppBuilder RegisterServices(this MauiAppBuilder mauiAppBuilder)
+        {
+            mauiAppBuilder.Services.AddTransient<IAiAssistant, LoadsheddingAiAssistant>();
+            mauiAppBuilder.Services.AddTransient<ISettings, ConstantSettings>();
+
+            // More services registered here.
+
+            return mauiAppBuilder;
+        }
+
+        public static MauiAppBuilder RegisterViewModels(this MauiAppBuilder mauiAppBuilder)
+        {
+            mauiAppBuilder.Services.AddSingleton<LoadsheddingQuestionViewModel>();
+            mauiAppBuilder.Services.AddSingleton<LoadsheddingAnswerViewModel>();
+
+            // More view-models registered here.
+
+            return mauiAppBuilder;
+        }
+
+        public static MauiAppBuilder RegisterViews(this MauiAppBuilder mauiAppBuilder)
+        {
+            mauiAppBuilder.Services.AddSingleton<LoadsheddingQuestionPage>();
+            mauiAppBuilder.Services.AddSingleton<LoadsheddingAnswerPage>();
+
+            // More views registered here.
+
+            return mauiAppBuilder;
         }
     }
 }
